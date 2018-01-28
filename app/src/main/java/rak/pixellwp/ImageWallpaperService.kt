6b18bfcc6497c80b34e1bc3e0d88612f4d0f23d2
iceburg333 <ice_burg333@yahoo.com>
2018-01-28 12:10:31 -0500

    adjust for orientation change

--- app/src/main/java/rak/pixellwp/ImageWallpaperService.kt
+++ app/src/main/java/rak/pixellwp/ImageWallpaperService.kt
@@ -31,11 +31,12 @@ class ImageWallpaperService : WallpaperService() {
         private var imageSrc = Rect(prefs.getInt("left", 0), prefs.getInt("top", 0), prefs.getInt("right", image.width), prefs.getInt("bottom", image.height))
         private var screenDimensions = Rect(imageSrc)
 
-        private var scaleFactor = 1f
+        private var scaleFactor = prefs.getFloat("scaleFactor", 1f)
         private val scaleDetector = ScaleGestureDetector(applicationContext, object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
             override fun onScale(detector: ScaleGestureDetector?): Boolean {
                 scaleFactor *= (detector?.scaleFactor ?: 1f)
                 scaleFactor = Math.max(0.1f, Math.min(scaleFactor, 5f))
+                prefs.edit().putFloat("scaleFactor", scaleFactor).apply()
                 return true
             }
         })
@@ -53,7 +54,6 @@ class ImageWallpaperService : WallpaperService() {
                         .putInt("top", imageSrc.top)
                         .putInt("right", imageSrc.right)
                         .putInt("bottom", imageSrc.bottom).apply()
-//                Log.d(Log.DEBUG.toString(), "prefs: ${prefs.all}")
                 return super.onScroll(e1, e2, distanceX, distanceY)
             }
         })
@@ -72,24 +72,28 @@ class ImageWallpaperService : WallpaperService() {
         }
 
         override fun onVisibilityChanged(visible: Boolean) {
-            Log.d(Log.DEBUG.toString(), "visibility changed")
             this.visible = visible
             if (visible) handler.post(drawRunner) else handler.removeCallbacks(drawRunner)
         }
 
         override fun onSurfaceDestroyed(holder: SurfaceHolder?) {
-            Log.d(Log.DEBUG.toString(), "surface destroyed")
             super.onSurfaceDestroyed(holder)
             this.visible = false
             handler.removeCallbacks(drawRunner)
         }
 
         override fun onSurfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
-            Log.d(Log.DEBUG.toString(), "surface changed")
             screenDimensions = Rect(0, 0, width, height)
-//            imageSrc = Rect(screenDimensions)
+            if (orientationHasChanged(width, height)) {
+                val right = imageSrc.left + imageSrc.height()
+                val bottom = imageSrc.top + imageSrc.width()
+                imageSrc = Rect(imageSrc.left, imageSrc.top, right, bottom)
+            }
             super.onSurfaceChanged(holder, format, width, height)
         }
+
+        private fun orientationHasChanged(width: Int, height: Int) =
+                (imageSrc.width() > imageSrc.height()) != (width > height)
 
         private fun draw() {
             var canvas: Canvas? = null
@@ -98,7 +102,6 @@ class ImageWallpaperService : WallpaperService() {
                 if (canvas != null){
                     canvas.drawColor(Color.BLACK)
                     canvas.drawBitmap(image, imageSrc, screenDimensions, null)
-                    Log.d(Log.DEBUG.toString(), "drew $imageSrc")
                 }
             } finally {
                 if (canvas != null) surfaceHolder.unlockCanvasAndPost(canvas)
