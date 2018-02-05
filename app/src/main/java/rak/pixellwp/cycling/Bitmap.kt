01fc7b98b1eb5ae4b8a6fbf22668704bfc10cbeb
iceburg333 <ice_burg333@yahoo.com>
2018-02-04 19:50:18 -0500

    optmized drawing, untested

--- app/src/main/java/rak/pixellwp/cycling/Bitmap.kt
+++ app/src/main/java/rak/pixellwp/cycling/Bitmap.kt
@@ -13,6 +13,8 @@ class Bitmap(img: ImgJson) {
     private val startTime = Date().time
     private val palette = Palette(img.getParsedColors(), img.cycles)
     private val pixels = img.pixels.toList()
+    private val optimizedPixels = optimizePixels(pixels)
+
     private val bitmap: Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
 
     init {
@@ -21,6 +23,29 @@ class Bitmap(img: ImgJson) {
 
     override fun toString(): String {
         return "image with dimensions $width x $height = ${width*height}, ${palette.colors.size} colors, ${palette.cycles.size} cycles and ${pixels.size} pixels"
+    }
+
+    private fun optimizePixels(pixels: List<Int>) : List<Pixel> {
+        val optPixels = mutableListOf<Pixel>()
+        val optColors = BooleanArray(pixels.size, { _ -> false}).toMutableList()
+
+        palette.cycles
+                .filter { it.rate != 0 }
+                .flatMap { it.low..it.high }
+                .forEach { optColors[it] = true }
+
+        var j = 0
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                //If this pixel references an animated color
+                if (optColors[pixels[j]]){
+                    optPixels.add(Pixel(x.toFloat(), y.toFloat(), j))
+                }
+                j++
+            }
+        }
+
+        return optPixels
     }
 
     private fun createBitmap() {
@@ -38,12 +63,16 @@ class Bitmap(img: ImgJson) {
         val timePassed = Date().time - startTime
         palette.cycle(timePassed)
 
-        //eventually optimize and only draw active pixels over old bitmap
+//        drawFullImage()
+        drawOptimizedImage()
+    }
+
+    private fun drawFullImage() {
         val canvas = Canvas(bitmap)
         val p = Paint()
 
         var j = 0
-        for (y in 0 until height){
+        for (y in 0 until height) {
             for (x in 0 until width) {
                 val color = palette.colors[pixels[j]]
                 p.color = color
@@ -51,8 +80,15 @@ class Bitmap(img: ImgJson) {
                 j++
             }
         }
+    }
 
-        bitmap.setPixel(300,300, Color.RED)
+    private fun drawOptimizedImage(){
+        val canvas = Canvas(bitmap)
+        val p = Paint()
+        for (pixel in optimizedPixels){
+            p.color = palette.colors[pixels[pixel.index]]
+            canvas.drawPoint(pixel.x, pixel.y, p)
+        }
     }
 
     fun render() : Bitmap{
---
+++ app/src/main/java/rak/pixellwp/cycling/Pixel.kt
@@ -0,0 +1,3 @@
+package rak.pixellwp.cycling
+
+data class Pixel(val x: Float, val y: Float, val index: Int)
