173cc2b92f39ed7dad05b8f26f328c1b6070c9e6
ManApart <r@austinkucera.com>
2022-02-04 10:36:27 -0500

    test organization

--- app/src/test/kotlin/rak.pixellwp/cycling/PaletteTest.kt
+++ app/src/test/kotlin/rak.pixellwp/cycling/PaletteTest.kt
@@ -22,7 +22,7 @@ class PaletteTest {
         val cycle = Cycle(0, 0, 1, 10)
         val colors = listOf(0,1,2,3,4,5,6,7,8,9,10,11)
         val shiftedColors = listOf(0,10,1,2,3,4,5,6,7,8,9,11)
-        val palette = Palette(colors, listOf(cycle))
+        val palette = Palette("", colors, listOf(cycle))
         val amount: Double = 1.0
 
         palette.shiftColors(palette.colors, cycle, amount)
--- app/src/test/kotlin/rak.pixellwp/cycling/timeline/blender/TimelineBlenderTest.kt
+++ app/src/test/kotlin/rak.pixellwp/cycling/timeline/blender/TimelineBlenderTest.kt
@@ -6,12 +6,12 @@ import rak.pixellwp.cycling.models.getSecondsFromHour
 
 fun createTimelineBlender() : TimelineBlender {
     val map = HashMap<Int, Palette>()
-    map[0] = Palette(listOf(), listOf())
+    map[0] = Palette("", listOf(), listOf())
     return TimelineBlender(map.entries.first())
 }
 
 fun createEntry(hour: Int) : Map.Entry<Int, Palette>{
     val map = HashMap<Int, Palette>()
-    map[getSecondsFromHour(hour)] = Palette(listOf(), listOf())
+    map[getSecondsFromHour(hour)] = Palette("", listOf(), listOf())
     return map.entries.first()
 }
