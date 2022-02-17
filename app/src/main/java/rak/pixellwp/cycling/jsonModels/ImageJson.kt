b42558d6dee897d0313a001a2ce7393d2f2de893
ManApart <r@austinkucera.com>
2022-02-16 21:36:19 -0500

    attempt to fix optimization

--- app/src/main/java/rak/pixellwp/cycling/jsonModels/ImageJson.kt
+++ app/src/main/java/rak/pixellwp/cycling/jsonModels/ImageJson.kt
@@ -4,12 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import rak.pixellwp.cycling.models.Cycle
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class ImageJson(val width: Int, val height: Int, val colors: List<ColorJson>, val cycles: List<Cycle>, val pixels: List<Int>){
-    fun getParsedColors() : List<Int> {
-        return colors.map { c ->  c.rgb}.toList()
+data class ImageJson(val width: Int, val height: Int, val colors: List<ColorJson>, val cycles: List<Cycle>, val pixels: List<Int>) {
+    fun getParsedColors(): List<Int> {
+        return colors.map { c -> c.rgb }.toList()
     }
 }
 
 fun defaultImageJson(): ImageJson {
-    return ImageJson(1,1, listOf(ColorJson(intArrayOf(0,0,0))), listOf(), listOf(0))
+    return ImageJson(640, 480, (0 until 307200).map { ColorJson(intArrayOf(0, 0, 0)) }, listOf(), Array(307200) { 0 }.toList())
 }
