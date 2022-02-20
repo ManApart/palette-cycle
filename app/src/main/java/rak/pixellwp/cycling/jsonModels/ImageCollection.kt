2f74f974f50401ac4990b0301969b9c89d718e4e
ManApart <r@austinkucera.com>
2022-02-20 10:13:35 -0500

    app starts

--- app/src/main/java/rak/pixellwp/cycling/jsonModels/ImageCollection.kt
+++ app/src/main/java/rak/pixellwp/cycling/jsonModels/ImageCollection.kt
@@ -1,10 +1,13 @@
 package rak.pixellwp.cycling.jsonModels
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import rak.pixellwp.cycling.wallpaperService.WeatherType
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class ImageCollection(val name: String, val images: List<ImageInfo>) {
-    constructor(id: String, name: String, month: String, script: String) : this(name, listOf(ImageInfo(name, id, month = month, script = script)))
+    @JsonCreator
+    constructor(name: String, id: String = "", month: String = "", script: String = "", weather: WeatherType = WeatherType.CLEAR, images: List<ImageInfo>?) : this(name, images ?: listOf(ImageInfo(name, id, month = month, script = script, weather = weather)))
 
     override fun toString(): String {
         return "Collection: $name" + images.map { it.toString() }
