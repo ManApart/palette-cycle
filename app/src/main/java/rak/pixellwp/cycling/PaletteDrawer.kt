d2eab8ab9b6c6404a109dd20a501cb8041fe96b5
ManApart <r@austinkucera.com>
2022-04-06 18:00:40 -0400

    thoughts and experiments

--- app/src/main/java/rak/pixellwp/cycling/PaletteDrawer.kt
+++ app/src/main/java/rak/pixellwp/cycling/PaletteDrawer.kt
@@ -5,62 +5,67 @@ import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
 import android.view.SurfaceHolder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import rak.pixellwp.cycling.models.PaletteImage
 import rak.pixellwp.cycling.wallpaperService.CyclingWallpaperService
 import rak.pixellwp.cycling.wallpaperService.getOffsetImage
 import java.util.*
+import kotlin.concurrent.thread
 import kotlin.math.floor
 
 class PaletteDrawer(private val engine: CyclingWallpaperService.CyclingWallpaperEngine, var image: PaletteImage) {
     private val logTag = "PaletteDrawer"
-    private var handlerThread = HandlerThread("drawThread")
     val id = System.currentTimeMillis()
 
-    init {
-        handlerThread.start()
-    }
-    private var handler = Handler(handlerThread.looper)
-    private val runner = Runnable { advanceAndDraw() }
     private val drawDelay = 20L
     private val startTime = Date().time
     private var visible = true
 
     fun startDrawing() {
         Log.v(logTag, "$id: Start drawing")
-        handlerThread.quitSafely()
-        handlerThread = HandlerThread("drawThread")
-        handlerThread.start()
-        handler = Handler(handlerThread.looper)
-        drawNow()
+        thread {
+            while (true) {
+                runBlocking {
+                    if (visible) {
+                        launch {
+                            try {
+                                advanceAndDraw()
+                            } catch (e: Throwable) {
+                                Log.e(logTag, "Failed to post, killing drawer + $id")
+                                Log.e(logTag, e.toString())
+                                stop()
+                            }
+                        }
+                    }
+                    delay(drawDelay)
+                }
+            }
+        }
     }
 
     fun drawNow() {
-        try {
-            handler.post(runner)
-        } catch (e: Throwable){
-            Log.e(logTag, "Failed to post, killing drawer + $id")
-            Log.e(logTag, e.toString())
-            stop()
-        }
+//        if (!this.visible) {
+        this.visible = true
+//            startDrawing()
+
+//        }
+
+
     }
 
     fun stop() {
         Log.v(logTag, "$id: Stop drawing")
         setVisible(false)
-        handlerThread.quitSafely()
     }
 
     fun setVisible(visible: Boolean) {
         this.visible = visible
         if (visible) {
             drawNow()
-        } else {
-            handler.removeCallbacks(runner)
         }
-    }
-
-    private fun drawAfterDelay() {
-        handler.postDelayed(runner, drawDelay)
     }
 
     private fun advanceAndDraw() {
@@ -79,8 +84,6 @@ class PaletteDrawer(private val engine: CyclingWallpaperService.CyclingWallpaper
             } catch (e: Exception) {
                 Log.e(logTag, "$id: failed to advanceAndDraw frame; probably couldn't lock surface")
             }
-            handler.removeCallbacks(runner)
         }
-        if (visible) drawAfterDelay()
     }
 }
